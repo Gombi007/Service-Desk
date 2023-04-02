ea38901f1d0b41d9e6cda19c4032c15378487060
Gombi007 <gombos88@gmail.com>
2023-04-02 14:06:00 +0200

    user was connected with spring user

--- Backend/src/main/java/com/gombisoft/servicedesk/models/DbUser.java
+++ Backend/src/main/java/com/gombisoft/servicedesk/models/DbUser.java
@@ -7,9 +7,11 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Document("users")
 @Getter
@@ -25,27 +27,26 @@ public class DbUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
