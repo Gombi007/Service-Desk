6397c796fa25df3bf35f9cb4d5f02e01dbac72e1
Gombi007 <gombos88@gmail.com>
2023-04-02 14:15:08 +0200

    user repository was updated

--- Backend/src/main/java/com/gombisoft/servicedesk/repositories/DbUserRepository.java
+++ Backend/src/main/java/com/gombisoft/servicedesk/repositories/DbUserRepository.java
@@ -2,6 +2,13 @@ package com.gombisoft.servicedesk.repositories;
 
 import com.gombisoft.servicedesk.models.DbUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface DbUserRepository extends MongoRepository<DbUser, Long> {
+import java.util.Optional;
+
+@Repository
+public interface DbUserRepository extends MongoRepository<DbUser, String> {
+    @Query("{username:'?0'}")
+    Optional<DbUser> findUserByUsername(String username);
 }
