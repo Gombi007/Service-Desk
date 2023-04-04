e9a6c87c91e099292b0f4b899246854ed1bf88f9
Gombi007 <gombos88@gmail.com>
2023-04-04 19:19:31 +0200

    new exception was created

---
+++ Backend/src/main/java/com/gombisoft/servicedesk/exceptions/NoSuchElementException.java
@@ -0,0 +1,7 @@
+package com.gombisoft.servicedesk.exceptions;
+
+public class NoSuchElementException extends RuntimeException {
+    public NoSuchElementException(String message) {
+        super(message);
+    }
+}
