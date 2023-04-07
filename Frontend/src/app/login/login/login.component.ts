2b1356c21f4f256c93077bed2c084f3c7977ab91
Gombi007 <gombos88@gmail.com>
2023-04-07 14:39:07 +0200

    login and register form design was done

--- Frontend/src/app/login/login/login.component.ts
+++ Frontend/src/app/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LANG_EN, LANG_HU } from '../../language.enum';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  showLoginForm: boolean = false;
+  lang: { [key: string]: string } = LANG_EN;
+
+  constructor(private languageService: LanguageService) { }
+
+  ngOnInit() {
+    this.updateLanguage();
+  }
+
+  setLanguage(lang: string) {
+    this.languageService.setLanguage(lang);
+    this.updateLanguage();
+  }
+
+  updateLanguage() {
+    this.lang = this.languageService.currentLanguage === 'en' ? LANG_EN : LANG_HU;
+  }
+
+  changeForm() {
+    this.showLoginForm = !this.showLoginForm;
+  }
 
 }
