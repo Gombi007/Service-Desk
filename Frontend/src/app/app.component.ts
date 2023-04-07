20fd9655626bd91da6ba6b99d95e693e3b6d1033
Gombi007 <gombos88@gmail.com>
2023-04-07 22:17:50 +0200

    check user logged in

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticateService } from './_services/authenticate.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'service-desk';
+  constructor(private authService: AuthenticateService) { }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 }
