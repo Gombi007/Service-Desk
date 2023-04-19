551ef45ad0f01af32b32b6ddc834abac62a1596c
Gombi007 <gombos88@gmail.com>
2023-04-19 21:03:37 +0200

    responsive design was updated

--- Frontend/src/app/tickets/detail-ticket/detail-ticket.component.ts
+++ Frontend/src/app/tickets/detail-ticket/detail-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 import { Ticket } from 'src/app/_models/ticket.model';
 
 @Component({
@@ -8,4 +8,20 @@ import { Ticket } from 'src/app/_models/ticket.model';
 })
 export class DetailTicketComponent {
   @Input() _ticket: Ticket | undefined;
+  @ViewChild('details', { read: ElementRef, static: false }) details!: ElementRef;
+  isFullscreen: boolean = false;
+
+  constructor(private renderer: Renderer2) { }
+
+  goFullscreen() {
+    this.renderer.setStyle(this.details.nativeElement, 'background-color', 'var(--backgound-color)');
+    document.documentElement.style.setProperty('--split-content-width-left', '0vw');
+    this.isFullscreen = true;
+  }
+
+  goCompactView() {
+    this.renderer.setStyle(this.details.nativeElement, 'background-color', 'var(--backgound-color)');
+    document.documentElement.style.setProperty('--split-content-width-left', '70vw');
+    this.isFullscreen = false;
+  }
 }
