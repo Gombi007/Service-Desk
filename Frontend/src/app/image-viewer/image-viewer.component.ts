c0561712fffa35c7e2fa84e69ff1ceac243f9382
Gombi007 <gombos88@gmail.com>
2023-04-19 11:22:19 +0200

    basic picture viewer was updated with thumbnails

--- Frontend/src/app/image-viewer/image-viewer.component.ts
+++ Frontend/src/app/image-viewer/image-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-viewer',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-viewer.component.scss']
 })
 export class ImageViewerComponent {
+  @Input() images: string[] = [];
 
 }
